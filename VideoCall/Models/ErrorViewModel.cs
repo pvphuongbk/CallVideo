28ef2acd9b8d4f172246b0de1cdd62c6414a73f5
doancong <Ceo@nhadat24h.net>
2025-06-30 08:55:52 +0700

    ok

---
+++ VideoCall/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VideoCall.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ VideoCall/Models/VideoCallHub.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+public class VideoCallHub : Hub
+{
+    public static List<string> ConnectedUsers = new List<string>();
+    public override async Task OnConnectedAsync()
+    {
+        ConnectedUsers.Add(Context.ConnectionId);
+
+        // Gửi ID của mình
+        await Clients.Caller.SendAsync("ReceiveYourId", Context.ConnectionId);
+
+        // Gửi danh sách user đang online cho người mới
+        await Clients.Caller.SendAsync("ReceiveUserList", ConnectedUsers);
+
+        // Thông báo với tất cả có user mới
+        await Clients.AllExcept(Context.ConnectionId).SendAsync("UserConnected", Context.ConnectionId);
+
+        await base.OnConnectedAsync();
+    }
+    public async Task CallUser(string connectionId)
+    {
+        await Clients.Client(connectionId).SendAsync("ReceiveCall", Context.ConnectionId);
+    }
+
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+        ConnectedUsers.Remove(Context.ConnectionId);
+
+        // Thông báo ngắt kết nối
+        await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+
+        await base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task SendOffer(string targetConnectionId, string offer)
+    {
+        await Clients.Client(targetConnectionId).SendAsync("ReceiveOffer", Context.ConnectionId, offer);
+    }
+
+    public async Task SendAnswer(string targetConnectionId, string answer)
+    {
+        await Clients.Client(targetConnectionId).SendAsync("ReceiveAnswer", Context.ConnectionId, answer);
+    }
+
+    public async Task SendIceCandidate(string targetConnectionId, string candidate)
+    {
+        await Clients.Client(targetConnectionId).SendAsync("ReceiveIceCandidate", Context.ConnectionId, candidate);
+    }
+
+}
